463f089486915ca2d069791f2073b993d6f72cc5
nik555max <nik555max@gmail.com>
2021-07-04 19:20:31 +0530

    retrofit with news api

--- app/src/main/java/com/nikhil/anand/retrofittutorial/JsonPlaceHolder.java
+++ app/src/main/java/com/nikhil/anand/retrofittutorial/JsonPlaceHolder.java
@@ -6,9 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-/*
-* Api Key : 0a5d17752d9f4ee29370cf0337956531
- */
 public interface JsonPlaceHolder {
 
     @GET("continents")
